05e00eb291a38b85e9dd66eb9f573d235169b6f6
Matthew Batchelder <borkweb@gmail.com>
2012-05-02 10:16:09 -0400

    config tweaks

--- config-example.js
+++ config-example.js
@@ -1,1 +1,6 @@
+process.env.bot_send_to = 'jabber@account.you.want.to.send.to.com';
+process.env.bot_listen_port = 8888;
+process.env.bot_jid = 'jabber@id.of.your.robot';
 process.env.bot_password = 'YOURBOTJABBERPASSWORD';
+process.env.bot_host = 'talk.google.com';
+process.env.bot_port = 5222;
